82aa40d085cc0a5d6794f3ba19bb85eec66f8d58
anikshajol <anikshajol@gmail.com>
2023-07-29 15:55:04 +0600

    practice task added

---
+++ Milestone-4-Javascript/module-16-intro-with-js/practice.js
@@ -0,0 +1,40 @@
+var giveHarrysMom = 1000;
+var oranges = 400;
+var apple = 300;
+
+var totalFruits2kg = oranges + apple;
+
+var returnMoney = giveHarrysMom - totalFruits2kg;
+
+// console.log(returnMoney);
+
+// 2nd task
+
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+
+var totalMarks = math + biology + chemistry + physics + bangla;
+
+var averageMarks = parseFloat((totalMarks / 5).toFixed(2));
+
+// console.log(averageMarks);
+
+// 3rd task
+
+var firstLine = "I am going to be";
+var secondLine = "an awesome developer";
+
+var oneLine = firstLine + " " + secondLine;
+
+// console.log(oneLine);
+
+//4th task
+
+var number1 = 119;
+var number2 = 5;
+
+var remainder = number1 % 5;
+console.log(remainder);
