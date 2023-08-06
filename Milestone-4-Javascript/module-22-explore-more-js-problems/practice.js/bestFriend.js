b6f4df1ca86298ca7286e168b68d3f0802c2304d
anikshajol <anikshajol@gmail.com>
2023-08-07 02:21:05 +0600

    practice added

--- Milestone-4-Javascript/module-22-explore-more-js-problems/practice.js/bestFriend.js
+++ Milestone-4-Javascript/module-22-explore-more-js-problems/practice.js/bestFriend.js
@@ -13,3 +13,33 @@ function bestFriend(friends) {
 const friends = ["sajid", "mamun", "kamal", "jubayer bin rased ", "chinku"];
 
 console.log(bestFriend(friends));
+
+function mindGame(number) {
+  if (typeof number !== "number") {
+    return "please provide a number";
+  } else if (number <= 0) {
+    return "please provide a positive number";
+  }
+  const resutl = (number * 3 + 10) / 2 - 5;
+  return resutl;
+}
+
+console.log(mindGame(55));
+
+function isLGSeven(number) {
+  if (typeof number !== "number") {
+    return " please provide a number";
+  } else {
+    const result = number - 7;
+    if (result > 7) {
+      const mul = number * 2;
+      return mul;
+    } else {
+      return result;
+    }
+  }
+}
+
+// console.log(isLGSeven(-15));
+
+console.log(findingBadData(array));
