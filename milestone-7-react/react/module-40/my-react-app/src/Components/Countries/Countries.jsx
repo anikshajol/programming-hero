0ce1eacc4435484c971471ca618fe170000c1c6e
anikshajol <anikshajol@gmail.com>
2023-09-10 17:44:20 +0600

    module40 complete

--- milestone-7-react/react/module-40/my-react-app/src/Components/Countries/Countries.jsx
+++ milestone-7-react/react/module-40/my-react-app/src/Components/Countries/Countries.jsx
@@ -5,6 +5,25 @@ import "./Countries.css";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
 
+  const [visitedCountries, setVisitedCountries]=useState([])
+
+  const [visitedFlags,setVisitedFlags]=useState([])
+
+  const handleVisitedFlags = (flags)=>{
+    console.log(flags);
+
+    const newVisitedFlags = [...visitedFlags,flags]
+
+    setVisitedFlags(newVisitedFlags)
+
+  }
+
+  const handleVisitedCountries =(country)=>{
+
+    const newVisitedCountires = [...visitedCountries,country]
+    setVisitedCountries(newVisitedCountires)
+  }
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
@@ -12,11 +31,26 @@ const Countries = () => {
   }, []);
 
   return (
-    <div className="grid">
+    <div>
       {/* <h1>{countries.length}</h1> */}
+
+    <h3>Visited Countries {visitedCountries.length} </h3>
+    <ul>
+      {
+        visitedCountries.map(country=><li key={country.cca2}>{country.name.common}</li>)
+        
+      }
+    </ul>
+<div>
+{
+        visitedFlags.map((flag,idx)=><img key={idx} style={{width:'50px'}} src={flag} alt="" />)
+      }
+</div>
+    <div className="grid">
       {countries.map((country) => (
-        <Country key={country.cca2} country={country}></Country>
+        <Country key={country.cca2} handleVisitedFlags={handleVisitedFlags} handleVisitedCountries={handleVisitedCountries} country={country}></Country>
       ))}
+      </div>
     </div>
   );
 };
